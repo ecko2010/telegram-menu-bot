9a46e3df90c88f32ca500c4170906729d57a91e2
ecko2010 <ahmedegy04@gmail.com>
2022-09-16 00:12:09 +0400

    Update botConfig.ts

--- src/config/botConfig.ts
+++ src/config/botConfig.ts
@@ -1,5 +1,5 @@
 // require("dotenv").config();
 const { Telegraf } = require("telegraf");
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+const bot = new Telegraf(process.env.5454164794:AAGDJ_mf2sCyNmTBPiFf91dWUD0JQdahWM8);
 
 export default bot;
